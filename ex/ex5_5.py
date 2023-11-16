e2a2435f21586f9f700102a3e5a3e99c33bebe88
sasaki0719 <y.sasaki.roppongi@gmail.com>
2023-11-16 14:42:22 +0900

    練習問題5-5

--- ex/ex5_5.py
+++ ex/ex5_5.py
@@ -3,17 +3,17 @@ def int_input(text):
     return int(input("支払い総額を入力してください >>"))
 
 # 割り勘を計算
-def calc_payment(amount,people=2):
-    
-    return pay
-dnum = amount /people
-pay = dnum // 100 * 100
-if dnum > pay:
-    pay = int(pay + 100)
-
 # 幹事の支払額の計算
-payorg = amount - pay * (people - 1)
+def calc_payment(amount,people=2):
+    dnum = amount /people
+    pay = dnum // 100 * 100
+    if dnum > pay:
+        pay = int(pay + 100)
+    payorg = amount - pay * (people - 1)
+    return pay,payorg
 
 # 結果の表示
-print("*** 支払額 ***")
-print(f"1人あたり{pay}円({people}人)、幹事は{dnum}円です")
+def show_payment(pay,payorg,people=2,):
+    print("*** 支払額 ***")
+    print(f"1人あたり{pay}円({people}人)、幹事は{payorg}円です")
+
